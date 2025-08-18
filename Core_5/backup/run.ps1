e9dae9bc86f32467d0b01e20f9cf1844a1ae92b2
Barbossnya <Barbossnya@hotmail.com>
2025-08-18 06:10:57 +0300

    Change

--- Core_5/backup/run.ps1
+++
@@ -1,8 +0,0 @@
-Write-Host "Compiling Java files..." -ForegroundColor Green
-javac src/*.java
-
-Write-Host "`nRunning the application..." -ForegroundColor Green
-java -cp src Main
-
-Write-Host "`nPress any key to continue..." -ForegroundColor Yellow
-$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown") 
--- Core_5/run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Compiling Java files...
-javac src/*.java
-echo.
-echo Running the application...
-java -cp src Main
-echo.
-pause 
